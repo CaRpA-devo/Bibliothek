6da6264174f27a021a381abbdd25d898aae54b36
Patrick Salgueiro <salgueiro2@web.de>
2025-01-27 15:36:34 +0100

    isert slider an iframes finished

---
+++ swiper.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper(".slider-wrapper", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickabel: true,
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1000: { slidesPerView: 3 },
+    },
+});
